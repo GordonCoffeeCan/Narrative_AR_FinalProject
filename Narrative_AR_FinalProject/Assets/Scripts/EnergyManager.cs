2ac91d288281d89d5a53c3a65b753c2af3339b67
GordonCoffeeCan <gordon.lee.6170@gmail.com>
2017-12-09 23:52:29 -0500

    project done. need test maybe?

--- Narrative_AR_FinalProject/Assets/Scripts/EnergyManager.cs
+++ Narrative_AR_FinalProject/Assets/Scripts/EnergyManager.cs
@@ -8,6 +8,8 @@ public class EnergyManager : MonoBehaviour {
     public static EnergyManager instance;
 
     [SerializeField] private MorphTree treeGrowth;
+    [SerializeField] private Transform Ground;
+    [SerializeField] private Transform treeParticle;
 
     [SerializeField] private Text totalEnergyNumber;
 
@@ -22,12 +24,10 @@ public class EnergyManager : MonoBehaviour {
     [SerializeField] private Slider exercisedSlider;
 
     [SerializeField] private Toggle smokedYes;
-    [SerializeField] private Toggle smokedNo;
 
     [SerializeField] private Toggle drinkedYes;
-    [SerializeField] private Toggle drinkedNo;
 
-    [SerializeField] private Button confirmButton;
+    [SerializeField] private Button newDayButton;
 
     [HideInInspector] public int nutrientsPoint;
 
@@ -44,36 +44,35 @@ public class EnergyManager : MonoBehaviour {
 	void Update () {
         if(nutrientsPoint != 0 && targetFound) {
             treeGrowth.currentGrowth = Mathf.MoveTowards(treeGrowth.currentGrowth, targetAmount, 0.002f);
+            Ground.localScale = new Vector3(1 + treeGrowth.currentGrowth * 4, 1 + treeGrowth.currentGrowth * 0.5f, 1 + treeGrowth.currentGrowth * 4);
+            treeParticle.localPosition = new Vector3(0, 0.5f + treeGrowth.currentGrowth * 5.5f, 0);
+            treeParticle.localScale = new Vector3(1 + treeGrowth.currentGrowth * 9, 1 + treeGrowth.currentGrowth * 9, 1 + treeGrowth.currentGrowth * 9);
             Debug.Log(treeGrowth.currentGrowth);
         }
     }
 
-    public void changeSleepHours() {
-        sleepHourNumber.text = sleepSlider.value.ToString() + " Hours";
+    public void ChangeSleepHours() {
+        sleepHourNumber.text = sleepSlider.value.ToString() + " HOURS";
     }
 
-    public void changeCaloriesInput() {
-        caloriesNumber.text = caloriesSlider.value.ToString() + " Cal";
+    public void ChangeCaloriesInput() {
+        caloriesNumber.text = caloriesSlider.value.ToString() + " CAL";
     }
 
-    public void changeExerciseTime() {
-        exercisedNumber.text = exercisedSlider.value.ToString() + " Minutes";
+    public void ChangeExerciseTime() {
+        exercisedNumber.text = exercisedSlider.value.ToString() + " MINUTES";
     }
 
-    public void changeSmokeYesToggle() {
-        smokedNo.isOn = !smokedYes.isOn;
-    }
-
-    public void changeSmokeNoToggle() {
-        smokedYes.isOn = !smokedNo.isOn;
-    }
-
-    public void changeDrinkYesToggle() {
-        drinkedNo.isOn = !drinkedYes.isOn;
-    }
-
-    public void changeDrinkNoToggle() {
-        drinkedYes.isOn = !drinkedNo.isOn;
+    public void OnNewDay() {
+        sleepSlider.value = 0;
+        caloriesSlider.value = 0;
+        exercisedSlider.value = 0;
+        smokedYes.isOn = false;
+        drinkedYes.isOn = false;
+        nutrientsPoint = 0;
+        totalEnergyNumber.text = nutrientsPoint.ToString() + " POINT";
+        newDayButton.gameObject.SetActive(false);
+        MenuPanel.gameObject.SetActive(true);
     }
 
     public void OnConfirm() {
@@ -89,7 +88,7 @@ public class EnergyManager : MonoBehaviour {
             nutrientsPoint += 2;
         }
 
-        if (exercisedSlider. value < 30 || exercisedSlider.value > 60) {
+        if (exercisedSlider.value < 30 || exercisedSlider.value > 60) {
             nutrientsPoint -= 1;
         } else if (exercisedSlider.value >= 30 || exercisedSlider.value <= 60) {
             nutrientsPoint += 2;
@@ -107,8 +106,9 @@ public class EnergyManager : MonoBehaviour {
             nutrientsPoint += 2;
         }
 
-        totalEnergyNumber.text = nutrientsPoint.ToString();
+        totalEnergyNumber.text = nutrientsPoint.ToString() + " POINT";
         MenuPanel.gameObject.SetActive(false);
+        newDayButton.gameObject.SetActive(true);
 
         targetAmount = treeGrowth.currentGrowth + (float)nutrientsPoint / 100;
     }
